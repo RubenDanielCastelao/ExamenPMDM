b974f77a90cd83bed680f1586795dff544abcd8f
RubenDanielCastelao <rnunezgonzalez@danielcastelao.org>
2023-10-25 09:50:44 +0200

    Funcionalidad y creacion de elementos

--- app/src/main/java/com/dam/contador/MyViewModel.kt
+++ app/src/main/java/com/dam/contador/MyViewModel.kt
@@ -7,24 +7,31 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
-    var num by mutableStateOf(0)
-    var namee by mutableStateOf("")
-    val numList = mutableListOf<Int>()
+    var numero by mutableStateOf(0)
+    var nombre by mutableStateOf("")
+    val numeroList = mutableListOf<Int>()
+    var ronda by mutableStateOf(0)
+    var startEstado by mutableStateOf("START")
 
 
     fun funcionRandom() {
 
-        num = (0..3).random()
-         numList.add(num)
+        numero = (0..3).random()
+         numeroList.add(numero)
 
         Log.d("Tag", "Estoy en el random")
     }
 
+    fun sumarRonda() {
+        ronda = ronda + 1
 
-    fun getNumero(): Int {
-        return num
+        Log.d("Tag", "Nueva ronda")
+    }
+
+    fun getNum(): Int {
+        return numero
     }
 fun getLista():List<Int>{
-    return numList
+    return numeroList
 }
 }
--- app/src/main/java/com/dam/contador/interfaz.kt
+++ app/src/main/java/com/dam/contador/interfaz.kt
@@ -1,14 +1,23 @@
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,33 +31,114 @@ fun IU(miViewModel: MyViewModel) {
 
 
     Column {
-
-        if (miViewModel.namee.length in 4..19) {
+        Text(
+            text = "RONDA",
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.End)
+        )
+        Modifier.padding(18.dp)
+        if(miViewModel.ronda > 9) {
             Text(
-                text = "Nombre: ${ miViewModel.namee}!",
+                text = miViewModel.ronda.toString(),
+                fontSize = 36.sp,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }else{
+            Text(
+                text = miViewModel.ronda.toString(),
                 fontSize = 24.sp,
+                modifier = Modifier.align(Alignment.End)
+            )
+        }
+        Row {
+            Button(
+                onClick = { Log.d("Tag", "Boton rojo") },
+                modifier = Modifier
+                    .size(64.dp)
+                    .background(Color.Blue)
+                    .padding(64.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
+
+            }
+
+            Button(
+                onClick = { Log.d("Tag", "Boton rojo") },
+                modifier = Modifier
+                    .size(64.dp)
+                    .background(Color.Green)
+                    .padding(64.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
+            }
+        }
+        Row {
+            Button(
+                onClick = { Log.d("Tag", "Boton rojo") },
+                modifier = Modifier
+                    .size(64.dp)
+                    .background(Color.Red)
+                    .padding(64.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
+            }
+
+            Button(
+                onClick = { Log.d("Tag", "Boton rojo") },
+                modifier = Modifier
+                    .size(64.dp)
+                    .background(Color.Yellow)
+                    .padding(64.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Color.White
+                )
+            ) {
+            }
+        }
+        Row {
+            Button(onClick = { miViewModel.startEstado = "RESET" },
+                modifier = Modifier.padding(50.dp)) {
+                    Text(text = miViewModel.startEstado)
+            }
+
+            Button(
+                onClick = {
+                    miViewModel.sumarRonda()
+                    Log.d("Tag", "Sumar Ronda")
+                },
+                modifier = Modifier.padding(64.dp)
+
+            )
+            {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_play_arrow_24),
+
+                    contentDescription = "Nuevo numero",
+                    Modifier.padding(8.dp)
 
                 )
-            Modifier.padding(18.dp)
+            }
         }
-        OutlinedTextField(
-            value = miViewModel.namee,
-            onValueChange = {
-                miViewModel.namee = it
-            },
-            label = { Text(text = "Name (Max 20 chars)") }
-        )
-
-
 
     }
 
-    Column {
-        Text(
-            text = "Último número: ${miViewModel.getNumero()}"+"\n Historial de números: ${miViewModel.getLista()}" ,
-            modifier = Modifier.padding(100.dp),
-            color = Color.DarkGray
-        )
+}
+
+
+
+
+
+/*
         Button(
             onClick = {
                miViewModel.funcionRandom()
@@ -66,15 +156,7 @@ fun IU(miViewModel: MyViewModel) {
 
             )
         }
-    }
-
-}
-
-
-
-
-
-
+ */
 
 
 
