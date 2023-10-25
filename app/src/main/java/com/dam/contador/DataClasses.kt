0af44e235866f2acf8465eed312bf95737d3a088
RubenDanielCastelao <rnunezgonzalez@danielcastelao.org>
2023-10-25 10:25:58 +0200

    Fixes

--- app/src/main/java/com/dam/contador/DataClasses.kt
+++ app/src/main/java/com/dam/contador/DataClasses.kt
@@ -3,7 +3,7 @@ package com.dam.contador
 class DataClasses {
 
     data class Nombre(val name: String) {
-        var nom: String = "Hola"
+        var nom: String = ""
     }
 
     data class NumeroActual(val name: String) {
